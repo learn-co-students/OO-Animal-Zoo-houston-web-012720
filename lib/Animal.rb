1759c2dbc101fe7b53f34ffebf0977ae9361c49b
DrVonDevious <drvondevious@gmail.com>
2020-01-31 23:33:22 -0600

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,27 @@
 class Animal
+  
+  attr_accessor :weight, :zoo, :species, :nickname, :zoo
+
+  @@all = []
+
+  def initialize(species, weight, nickname, zoo = nil)
+    self.species = species
+    self.weight = weight
+    self.nickname = nickname
+    self.zoo = zoo
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_to_zoo(zoo)
+    @zoo = zoo
+  end  
+
+  def self.find_by_species(species)
+    @@all.select { |animal| animal.species == species }
+  end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,38 @@
 class Zoo
+ 
+  attr_accessor :animals, :location, :name
+
+  @@all = []
+
+  def initialize(name, location)
+    self.name = name
+    self.location = location
+    self.animals = []
+    @@all << self
+  end  
+
+  def self.all
+    @@all
+  end  
+
+  def animals
+    Animal.all.select { |animal| animal.zoo == self }
+  end
+
+  def animal_species
+    @@all.map { |animal| animal.species }.uniq
+  end
+
+  def find_by_species(species)
+    self.animals.map { |animal| animal.species }.uniq
+  end
+
+  def animal_nicknames
+    self.animals.map { |animal| animal.nickname }
+  end
+
+  def self.find_by_location(location)
+    @@all.select { |zoo| zoo.location == location}
+  end  
 
 end
