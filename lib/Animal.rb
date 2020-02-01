e58010dfa8deb9335afa78bdbd37eae40e055fbb
somaia-khalil <sumaya_khair@live.com>
2020-02-01 17:55:25 -0600

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
+    @@all = []
+    attr_accessor :weight
+    attr_reader :species , :nickname , :zoo
+    def initialize(species,weight,nickname,zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+        @zoo = zoo
+    end
+    def self.all
+        @@all
+    end
+    def self.find_by_species(sp)
+        @@all.select do |instance|
+            instance.species == sp
+        end
+
+    end
+
 
 end
--- run.rb
+++ run.rb
@@ -1,9 +1,14 @@
 require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
-require 'pry'
+require'pry'
 
 
-#Test your code here
+
+tof = Zoo.new("ha", "germany")
+yof = Zoo.new("ha", "uermany")
+
+dog = Animal.new("dog",4,"tra",tof)
+cat = Animal.new("cat",4,"fat",yof)
 
 
 binding.pry
