c96785c92fd487ab1c911bbc64bf8f914b0c02ff
GianMoran <gpmoran@uh.edu>
2020-02-02 15:17:53 -0600

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,25 @@
-class Zoo
+class Zoo 
+    attr_accessor :name, :location 
+    @@all = []
+    def initialize(name,location)
+    @name = name 
+    @location = location
+    @@all << self
+    end
+    def animals 
+    Animals.all.select{|animal| animal.zoo == self}
+    end
+    def animal_species 
+     animals.map{|animal| animal.species}.uniq
+    end
+    def find_by_species(animal_species)
+        animals.map{|animal| animal.species == animal_species}
+    end
+    def animal_nicknames 
+        animals.map{|animal|animal.nicknames}
+    end
+    def find_by_location(location)
+        @@all.select{|zoo| zoo.location == location}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,23 @@ require 'pry'
 
 
 #Test your code here
+zoo1 = Zoo.new("houston", "houston zoo")
+zoo2 = Zoo.new("dallas", "dallas zoo")
+zoo3 = Zoo.new("austin", "austin zoo")
 
+animal1 = Animal.new("Rage", 420, "lion", zoo1)
+animal2 = Animal.new("Leo", 320,  "lion", zoo1)
+animal3 = Animal.new("Max", 350, "gorilla", zoo1)
+animal4 = Animal.new("Ash", 350, "gorilla", zoo1)
 
+animal5 = Animal.new("Venom",2, "rattlesnake", zoo2)
+animal6 = Animal.new("Fangs",2, "rattlesnake", zoo2)
+animal7 = Animal.new("Tikki",680, "white tiger", zoo2)
+animal8 = Animal.new("Takki", 370, "south africa tiger", zoo2)
+
+animal9 = Animal.new("Spitty",2, "spit cobra", zoo3)
+animal10 = Animal.new("Choker",2, "python", zoo3)
+animal11 = Animal.new("Lucy",440, "south american tiger", zoo3)
+animal12 = Animal.new("alf", 310, "south africa tiger", zoo3)
 binding.pry
 puts "done"
