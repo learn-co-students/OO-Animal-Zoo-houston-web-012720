428d220a27be38bfb48c00335082697e17efc104
timothyalton <mitchell.alton@live.com>
2020-02-03 08:24:00 -0600

    Done.

--- run.rb
+++ run.rb
@@ -3,7 +3,15 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+
+
+z1 = Zoo.new("Houston Zoo", "Houston, TX")
+z2 = Zoo.new("San Diego Zoo", "San Diego, CA")
+z3 = Zoo.new("Central Park Zoo", "New York City, NY")
+
+a1 = Animal.new("Lion", 450, "Leo", z1)
+a2 = Animal.new("Zebra", 800, "Stripes", z2)
+a3 = Animal.new("Elephant", 13000, "Dumbo", z3)
 
 
 binding.pry
