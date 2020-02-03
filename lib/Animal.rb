30b3cfc989bd4f0af390d42edfeea7bcf669b786
corn-cloud <comowaiye@gmail.com>
2020-02-03 17:23:56 -0600

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,40 @@
 class Animal
+    attr_accessor :weight
+    attr_reader :nickname, :species, :zoo
+    @@all = []
+
+    def initialize(nickname, species, weight, zoo)
+        @nickname = nickname
+        @species = species
+        @weight = weight 
+        @zoo = zoo 
+        @@all << self 
+    end
+
+
+
+    # def nickname
+    #     @nickname
+    # end
+
+    # def species
+    #     @species
+    # end
+
+    # def weight
+    #     @weight
+    # end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        @@all.select {|a| a.species == species}
+        end
+        
+
+
+    
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,55 @@
 class Zoo
+    attr_accessor :location, :name
+    @@all = []
+
+    def initialize(location, name)
+        @location = location
+        @name = name
+        @@all << self
+    end
+
+    def name
+        @name 
+    end
+
+    def location 
+        @location 
+    end
+
+    def self.all
+        @@all
+
+    end
+
+    def animals
+        Animal.all.select do |a| 
+            a.zoo == self
+        end
+    end
+
+
+    def animal_species
+    #    Animal.all.map {|a| a.species == self}
+    animals.map{|a| a.species}.uniq
+    end
+
+    def find_by_species(species)
+        animals.map {|s| s.species == species}
+    end
+
+    def animal_nicknames 
+        animals.map {|a| a.nickname}
+    end
+
+    def self.find_by_location(location)
+        @@all.find {|z| z.location}
+    end
+
+    
+
+    
+
+    
+
 
 end
